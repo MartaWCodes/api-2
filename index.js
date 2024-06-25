02d8b4da7431cb05bd5de557a8b339aa88d5a794
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-06-25 19:19:55 +0100

    Wire up endpoint to DB data

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 // require('dotenv').config();
 
 const app = express();
 
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'customer'
+})
+
 app.use(express.json());
 
 app.get('/user', (req, res) => {
   // Return all entries from the users table in the DB
-  res.json({
-    message: 'TODO',
-  });
+  pool.query('select * from users', (err, result) => {
+    res.json(result);
+    // TODO: err
+  })
 });
 
 const port = 5000; // process.env.PORT || 5000;
